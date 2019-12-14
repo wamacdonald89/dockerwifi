35405ade5d06e62e0bc321200bef5c8a4a6f71ff
Andy <wamacdonald89@gmail.com>
2019-12-14 16:46:21 -0500

    check for WEP

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 # Defaults
 HW_MODE=g # a,b,g,n
 BAND="2.4GHz"
@@ -25,10 +24,15 @@ iptables -t nat -D POSTROUTING -s ${SUBNET}/24 -j MASQUERADE > /dev/null 2>&1 ||
 iptables -t nat -A POSTROUTING -s ${SUBNET}/24 -j MASQUERADE
 echo -e "${BLUE}[INFO]${NC} NAT POSTROUTING ${GREEN}${SUBNET}/24$ MASQUERADE${NC}"
 
-
 if [ ${CHANNEL} -gt 14 ]; then
   HW_MODE=a
   BAND="5GHz"
+fi
+
+# Check for WEP
+if grep -q "wep_key" /etc/hostapd.conf
+then
+  KEYMGT="WEP"
 fi
 
 # TODO EAP prep
@@ -40,6 +44,7 @@ if [[ $KEYMGT == "EAP" ]]; then
   ln -s server.pem ca.pem
 fi
 
+
 echo "[+] Configuring hostapd..."
 export IFACE=${IFACE}
 export HW_MODE=${HW_MODE}
@@ -50,7 +55,7 @@ rm /tmp/hostapd.conf
 SSID=$(cat /etc/hostapd.conf | grep "ssid" | cut -d"=" -f2)
 BAND=$(cat /etc/hostapd.conf | grep "hw_mode" | cut -d"=" -f2)
 CHANNEL=$(cat /etc/hostapd.conf | grep "channel" | cut -d"=" -f2)
-PASSPHRASE=$(cat /etc/hostapd.conf | grep "wpa_passphrase" | cut -d"=" -f2)
+PASSPHRASE=$(cat /etc/hostapd.conf | grep -E "wpa_passphrase|wep_key" | cut -d"=" -f2)
 
 echo "[+] Configuring DHCP server..."
 
@@ -72,14 +77,15 @@ echo "[+] Starting DHCP server .."
 dhcpd ${IFACE} &> /dev/null
 
 echo "Starting HostAP daemon ..."
-
+echo -e "${BLUE}[INFO]${NC} Key Mgmt:\t${GREEN}$KEYMGT${NC}"
 echo -e "${BLUE}[INFO]${NC} Interface:\t${GREEN}$IFACE${NC}"
 echo -e "${BLUE}[INFO]${NC} SSID:\t\t${GREEN}$SSID${NC}"
 echo -e "${BLUE}[INFO]${NC} Frequency Band:\t${GREEN}$BAND${NC}"
 echo -e "${BLUE}[INFO]${NC} Channel:\t\t${GREEN}$CHANNEL${NC}"
 if [ $KEYMGT == "PSK" ]; then
   echo -e "${BLUE}[INFO]${NC} Passphrase:\t${GREEN}$PASSPHRASE${NC}"
+elif [ $KEYMGT == "WEP" ]; then
+  echo -e "${BLUE}[INFO]${NC} WEP Key:\t\t${GREEN}$PASSPHRASE${NC}"
 fi
-
 echo -e "${BLUE}[INFO]${NC} Press CTRL-C to stop..."
 /usr/sbin/hostapd /etc/hostapd.conf
